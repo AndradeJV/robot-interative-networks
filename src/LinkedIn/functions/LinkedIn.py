48eea3df6b8fe101967a9b6aa31fc1cc4de034af
Joao Andrade <joaovandrade30@gmail.com>
2021-11-23 11:38:31 -0300

    Início da criação do módulo de envio de mensagem

--- src/LinkedIn/functions/LinkedIn.py
+++ src/LinkedIn/functions/LinkedIn.py
@@ -17,11 +17,41 @@ class LinkedIn():
 
   def click_button_login():
     driver.find_element_by_css_selector(".babybear\:pt-\[24px\] > div > div > form > button").click()
+    time.sleep(1)
+
+
+  def click_button_mensagens():
+    driver.find_element_by_css_selector("#ember24 > span").click()
+    time.sleep(2)
+
+
+  def click_button_nova_mensagem():
+    driver.find_element_by_xpath("/html/body/div[5]/div[3]/div[2]/div/div/main/div/section[1]/div[1]/div[1]/a").click()
+    time.sleep(2)
+
+
+  def click_button_enviar_mensagem():
+    driver.find_element_by_css_selector(".align-items-center > div:nth-child(1) > button").click()
+    time.sleep(1)
+
+
+  def click_button_enviar_mensagem_via_pesquisa():
+    driver.find_element_by_css_selector("#ember43").click()
+
+  
+  def click_usuario_pesquisado():
+    driver.find_element_by_css_selector("div.entity-result__content.pt3.pb3.t-12.t-black--light > div.t-roman.t-sans > div > span.entity-result__title-line.flex-shrink-1.entity-result__title-text--black > span > a > span > span:nth-child(1)").click()
+    time.sleep(2)
+
+
+  def click_button_enviar_msg_via_perfil():
+    driver.find_element_by_css_selector(".pt2.display-flex > div > a").click()
 
 
   def preencher_email(email):
     driver.find_element_by_css_selector("#session_key").send_keys(email)
     time.sleep(2)
+
 
   def preencher_celular(celular):
     driver.find_element_by_css_selector("#session_key").send_keys(celular)
@@ -43,6 +73,28 @@ class LinkedIn():
     time.sleep(1)
 
 
+  def pesquisar_usuario_via_pagina_inicial(usuario):
+    driver.find_element_by_css_selector("#global-nav-typeahead > input").send_keys(usuario)
+    time.sleep(1)
+    driver.find_element_by_css_selector("#global-nav-typeahead > input").send_keys(Keys.ENTER)
+
+
+  def pesquisar_usuario_via_mensagem(usuario):
+    driver.find_element_by_xpath("/html/body/div[5]/div[3]/div[2]/div/div/main/div/section[2]/div[1]/div[2]/section/div/input").send_keys(usuario)
+    time.sleep(1)
+    driver.find_element_by_xpath("/html/body/div[5]/div[3]/div[2]/div/div/main/div/section[2]/div[1]/div[2]/section/div/input").send_keys(Keys.ENTER)
+    time.sleep(1)
+
+
+  def enviar_mensagem_via_pesquisa(mensagem):
+    driver.find_element_by_css_selector("#msg-form-ember82 > div.msg-form__msg-content-container.msg-form__message-texteditor.relative.flex-grow-1.display-flex > div > div.flex-grow-1 > div.msg-form__contenteditable.t-14.t-black--light.t-normal.flex-grow-1.full-height.notranslate").send_keys(mensagem)
+
+
+  # Necessita de manutenção
+  def enviar_mensagem_textual_via_mensagens(mensagem):
+    driver.find_element_by_css_selector(".full-height.notranslate").send_keys(mensagem)
+
+
   def validar_url_pag_inicial():
     with open("src/LinkedIn/fixtures/urls.json") as f_urls:
       data_urls = json.load(f_urls)
@@ -55,7 +107,7 @@ class LinkedIn():
 
 
     if url_local == url_validar:
-      print(f'URL validada com sucesso: {url_local}')
+      print(f'URL página inicial validada com sucesso: {url_local}')
 
     else:
       driver.quit()
@@ -71,7 +123,7 @@ class LinkedIn():
 
 
     if url_local == url_validar:
-      print(f'URL validada com sucesso: {url_local}')
+      print(f'URL perfil validada com sucesso: {url_local}')
 
     else:
       driver.quit()
--- src/LinkedIn/specs_linkedin.py
+++ src/LinkedIn/specs_linkedin.py
@@ -16,7 +16,20 @@ def logar_linkedin():
   lk.validar_url_perfil_pessoal()
   lk.go_to_pagina_inicial()
   lk.validar_url_pag_inicial()
-  # lk.encerrar_sessao()
-  lk.mensagem_sucesso()
 
-logar_linkedin()
+
+def enviar_mensagem():
+  logar_linkedin()
+  lk.pesquisar_usuario_via_pagina_inicial("Isabelly Vitorino")
+  lk.click_usuario_pesquisado()
+  lk.click_button_enviar_msg_via_perfil()
+  lk.enviar_mensagem_via_pesquisa("Teste automatizado")
+  # lk.click_button_enviar_mensagem()
+  lk.click_button_enviar_mensagem()
+
+
+enviar_mensagem()
+
+
+lk.encerrar_sessao()
+lk.mensagem_sucesso()
